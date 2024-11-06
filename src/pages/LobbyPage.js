1043962f97662bdc5f7a370de08e8c149909fd58
Heonseok Lee <heolee@c2r6s4.42seoul.kr>
2024-11-06 16:59:11 +0900

    [ADD] : layout end

--- src/pages/LobbyPage.js
+++ src/pages/LobbyPage.js
@@ -8,10 +8,38 @@ export default class Lobby extends Component {
                 <button class="m-4 border-0 fs-5 text-white fw-bold rounded-pill" style="width: 9rem; height: 3rem; background-color: rgba(14, 180, 252, 0.6);">42 PONG</button>
                 <button class="m-4 ms-auto border-0 fs-5 text-white fw-bold rounded-pill" style="width: 9rem; height: 3rem; background-color: rgba(14, 180, 252, 0.6);">Make room</button>
             </header>
-            <div class="p-5" style="width: 100%; height: 88vh;">
-                <div class="p-5 rounded-5" style="width: 100%; height: 100%; background-color: rgba(14, 180, 252, 0.25);">
-                    <ul class="row row-cols-3 list-unstyled">
-                        <li class="col m-auto rounded-5" style="width: 16rem; height: 12rem; background-color: rgba(14, 180, 252, 0.5);">
+            <div class="parent" style="">
+                <div class="parent" style="height: 100%; background-color: rgba(14, 180, 252, 0.25);">
+                    <ul class="container">
+                        <li class="col m-auto rounded-5 d-flex flex-column p-3" style="width: 16rem; height: 12rem; background-color: rgba(14, 180, 252, 0.5);">
+                            <div class="my-auto fs-5 text-white">2/4</div>
+                            <div class="m-auto rounded-circle" style="width: 5rem; height: 5rem; background-color: white"></div>
+                            <div class="m-auto fs-4 text-white">heolee's room</div>
+                        </li>
+                        <li class="col m-auto rounded-5 d-flex flex-column p-3" style="width: 16rem; height: 12rem; background-color: rgba(14, 180, 252, 0.5);">
+                            <div class="my-auto fs-5 text-white">2/2</div>
+                            <div class="m-auto rounded-circle" style="width: 5rem; height: 5rem; background-color: white"></div>
+                            <div class="m-auto fs-4 text-white">haejeong's room</div>
+                        </li>
+                        <li class="col m-auto rounded-5 d-flex flex-column p-3" style="width: 16rem; height: 12rem; background-color: rgba(14, 180, 252, 0.5);">
+                            <div class="my-auto fs-5 text-white">1/2</div>
+                            <div class="m-auto rounded-circle" style="width: 5rem; height: 5rem; background-color: white"></div>
+                            <div class="m-auto fs-4 text-white">sangyhan's room</div>
+                        </li>
+                        <li class="col m-auto rounded-5 d-flex flex-column p-3" style="width: 16rem; height: 12rem; background-color: rgba(14, 180, 252, 0.5);">
+                            <div class="my-auto fs-5 text-white">1/4</div>
+                            <div class="m-auto rounded-circle" style="width: 5rem; height: 5rem; background-color: white"></div>
+                            <div class="m-auto fs-4 text-white">sham's room</div>
+                        </li>
+                        <li class="col m-auto rounded-5 d-flex flex-column p-3" style="width: 16rem; height: 12rem; background-color: rgba(14, 180, 252, 0.5);">
+                            <div class="my-auto fs-5 text-white">1/4</div>
+                            <div class="m-auto rounded-circle" style="width: 5rem; height: 5rem; background-color: white"></div>
+                            <div class="m-auto fs-4 text-white">klha's room</div>
+                        </li>
+                        <li class="col m-auto rounded-5 d-flex flex-column p-3" style="width: 16rem; height: 12rem; background-color: rgba(14, 180, 252, 0.5);">
+                            <div class="my-auto fs-5 text-white">3/4</div>
+                            <div class="m-auto rounded-circle" style="width: 5rem; height: 5rem; background-color: white"></div>
+                            <div class="m-auto fs-4 text-white">dna's room</div>
                         </li>
                     </ul>
                 </div>
--- src/pages/RoomTwoPage.js
+++ src/pages/RoomTwoPage.js
@@ -29,8 +29,8 @@ export default class RoomTwo extends Component {
                             <div class="d-flex align-items-center" style="height: 20%;">heolee : 어우 개 힘드네</div>
                             <div class="d-flex align-items-center" style="height: 20%; border-bottom: 2px solid #0EB4FC;">heolee : 어우 개 어렵네</div>
                             <form class="d-flex justify-content-evenly align-items-center" style="height: 20%;">
-                                <input style="width: 80%;"></input>
-                                <input type="submit" value="Enter">
+                                <input class="border-0" style="width: 80%;"></input>
+                                <input class="border-0 text-white" style="background-color: rgba(14, 180, 252, 0.6);" type="submit" value="Enter">
                             </form>
                         </div>
                         <button class="m-auto border-0 fs-1 text-white fw-bold rounded-pill" style="width: 15rem; height: 6rem; background-color: rgba(14, 180, 252, 0.6);">Start</button>
