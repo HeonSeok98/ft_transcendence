264e7f4ee9dfe9dc83d1d7321e6b13837baae6dc
이헌석 <109660331+HeonSeok98@users.noreply.github.com>
2024-12-11 14:53:15 +0900

    [Fix] : 홈 반응형

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,12 +16,12 @@ export default class HomePage extends Component {
             <div class="p-5" style="width: 100%; height: 88vh;">
                 <div class="home-container d-flex rounded-5 p-3" style="width: 100%; height: 100%; background-color: rgba(14, 180, 252, 0.25); overflow: auto;">
                     <div class="home-item-container d-flex flex-column">
-                        <button class="home-button border-0 fs-1 text-white fw-bold rounded-pill custom-button" style="width: 18rem; height: 6rem;" id="gamelog-button">gamelog</button>
-                        <button class="home-button border-0 fs-1 text-white fw-bold rounded-pill custom-button" style="width: 18rem; height: 6rem;" id="lobby-button">lobby</button>
-                        <button class="home-button border-0 fs-1 text-white fw-bold rounded-pill custom-button" style="width: 18rem; height: 6rem;" id="select-button">make room</button>
+                        <button class="home-button border-0 fs-1 text-white fw-bold rounded-pill custom-button" id="gamelog-button">gamelog</button>
+                        <button class="home-button border-0 fs-1 text-white fw-bold rounded-pill custom-button" id="lobby-button">lobby</button>
+                        <button class="home-button border-0 fs-1 text-white fw-bold rounded-pill custom-button" id="select-button">make room</button>
                     </div>
                     <div class="home-item-container d-flex">
-                        <div data-component="usercard" class="m-auto" style="width: 22rem; height: 30rem;">
+                        <div data-component="usercard" class="home-card m-auto">
                         </div>
                     </div>
                 </div>
